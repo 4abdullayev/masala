3e1eebda632816021a6114c13499d03fb0b4bbf0
Asadullo <asadulloabdullayev404@gmail.com>
2023-02-14 09:16:44 +0500

    first commit

---
+++ function.py
@@ -0,0 +1,40 @@
+# def sumoflist(a):
+#     sum=0
+#     for i in lst:
+#         sum+=i
+#     print(sum)
+# lst=[3,5,2,8]
+# sumoflist(lst)
+
+# def salom(d1,d2):
+#     for i in d1:
+#         if i in d2:
+#             d1[i]+=d2[i]
+#         else :
+#             d1.update(d2)
+#     print(d1)        
+
+# d1={'a':100,'b':200,'c':300}
+# d2={'a':300,'b':200,'d':400}
+# salom(d1,d2)
+
+# 1-masala
+# def nimadur(a):
+#     d={}
+#     for i in a:
+#         d[i]=a.count(i)
+#     print(d)
+
+# a="w3resource"
+# nimadur(a)
+
+# 2-masala
+# def teskari(a):
+# a=123
+# teskari(a)
+
+a=int(input())
+if a<5:
+  print(0)
+else :
+  print(1)
